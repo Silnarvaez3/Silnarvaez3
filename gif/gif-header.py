31cd99331aca5a6966b254791c7b845f53f16718
Silvio <silnarvaez.sn@gmail.com>
2023-06-09 00:20:52 -0400

    resize gif

--- gif/gif-header.py
+++ gif/gif-header.py
@@ -3,10 +3,22 @@ from PIL import Image, ImageSequence
 # Crea una lista con las imágenes individuales que se utilizarán para crear el GIF
 frames = []
 
-# Agrega las imágenes a la lista (asegúrate de que todas las imágenes tengan el mismo tamaño)
-frames.append(Image.open('holamundo.png'))
-frames.append(Image.open('helloworld.png'))
-frames.append(Image.open('hallowelt.jpg'))
+
+# Tamaño deseado para las imágenes (ancho, alto)
+size_deseado = (300, 300)
 
 
-frames[0].save('animacion.gif', format='GIF', append_images=frames[1:], save_all=True, duration=200, loop=0)
+# Agrega las imágenes a la lista después de redimensionarlas al tamaño deseado
+imagen1 = Image.open('holamundo.png').resize(size_deseado)
+imagen2 = Image.open('helloworld.png').resize(size_deseado)
+imagen3 = Image.open('hallowelt.jpg').resize(size_deseado)
+
+frames.append(imagen1)
+frames.append(imagen2)
+frames.append(imagen3)
+
+
+
+
+
+frames[0].save('animacion.gif', format='GIF', append_images=frames[1:], save_all=True, duration=500, loop=0)
